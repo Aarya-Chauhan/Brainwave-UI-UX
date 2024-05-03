ef05c0463451896c3664249b8f4ad184bb7ba1ad
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-05-03 12:29:24 +0530

    starter code init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import "./App.css";
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
 
-function App() {
+const App = () => {
   return (
     <>
       <h1 className="text-3xl font-bold underline">"hello world!!"</h1>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Button className="mt-10" href="#login"></Button>
+      </div>
+      <ButtonGradient />
     </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Button.jsx
@@ -0,0 +1,5 @@
+const Button = ({ className, href, onClick, children, px, white }) => {
+  const renderButton = () => <button></button>;
+};
+
+export default Button;
