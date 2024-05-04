2b8926e92733a1d67d5be1ce577fd115c44d3963
Aarya Chauhan <aaryachauhan11@gmail.com>
2024-05-04 11:09:13 +0530

    adding benefits.js

---
+++ src/components/Benefits.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { benefits } from "../constants";
+import Heading from "./Heading";
+import Section from "./Section";
+import Arrow from "../assets/svg/Arrow";
+import { GradientLight } from "./design/Benefits";
+import ClipPath from "../assets/svg/ClipPath";
+
+const Benefits = () => {
+  return (
+    <section id="features">
+      <div className="container relative z-2">
+        <Heading
+          className="md:max-w-md lg:max-w-2xl"
+          title="chat Smarter, Not Harder wit Brainwave "
+        />
+      </div>
+    </section>
+  );
+};
+
+export default Benefits;
